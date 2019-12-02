eb5be68835736678cb4f16d5b102fc4c7c256784
Anjan kumar <anjan.kumar@vertiv.com>
2019-12-02 14:15:35 +0000

    complete merge

--- 2019/day1/base.py
+++ 2019/day1/base.py
@@ -9,7 +9,6 @@ import os
 import csv
 
 PWD = os.getcwd()
-<<<<<<< HEAD
 
 total_fuel = 0
 module_fuel = 0
@@ -33,17 +32,3 @@ def solve():
             module_fuel = 0
     print(net_fuel)
     return (net_fuel)
-=======
-total_fuel = 0
-
-def get_fuel_estimate(mass):
-    fuel_required = ((int(mass)/3) -2)
-    return fuel_required
-
-with open(PWD +'/2019/day1/input.txt', 'r') as input_data:
-    input_reader = csv.reader(input_data)
-    for row in input_reader:
-        total_fuel += get_fuel_estimate(row[0])
-
-print(total_fuel)
->>>>>>> 3f838b8a5d0817b299a0542ce143cbaa47fab973
